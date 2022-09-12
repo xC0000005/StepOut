48f8a9d9c0f5af46f5e79665fc5e16e013763607
J.C. Nelson <32139633+xC0000005@users.noreply.github.com>
2022-09-12 08:50:12 -0700

    Output port status.

---
+++ readme.md
@@ -0,0 +1,3 @@
+Reads input signals from a fixed stepper driver coil and translates them into enable/step/dir for external drivers, allowing a fixed stepper board to drive an external heavy duty stepper.
+
+D2-D5 are inputs (A, B, A2, B2) from the optocoupler.
--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,35 @@
 #include <arduino.h>
 #include <pins_arduino.h>
 
+// The coil input pins should be on the same port - this uses D2-5
+#define COIL_B  4  // White wire in my setup. - D2 in my setup
+#define COIL_A  8  // Red wire in my setup    - D3 in my setup
+#define COIL_A2 16 // Green wire in my setup  - D4 on my setup
+#define COIL_B2 32 // Black wire in my setup  - D5 on my setup
+#define COIL_MASK (COIL_A | COIL_B | COIL_A2 | COIL_B2)
+
 byte old_pin_status = 0;
 
 void setup() {
   Serial.begin(115200);
-  for (int i = 1; i < 8; i++)
+  for (int i = 2; i < 8; i++)
       pinMode(i, INPUT);
 }
 
 void loop() {
-  byte pin_status = PIND;
+  byte pin_status = PIND & COIL_MASK;
   if (pin_status != old_pin_status) {
     Serial.print("Port Status:");
     Serial.println(pin_status, HEX);
+    if (pin_status & COIL_A)
+       Serial.println("A");
+    if (pin_status & COIL_B)
+       Serial.println("B");
+    if (pin_status & COIL_A2)
+       Serial.println("A2");
+    if (pin_status & COIL_B2)
+       Serial.println("B2");
+
     old_pin_status = pin_status;
   }
 
