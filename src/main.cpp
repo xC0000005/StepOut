0a3d87dd0fd679cccbd8beacb46909c3e641e40e
J.C. Nelson <32139633+xC0000005@users.noreply.github.com>
2022-09-12 21:13:45 -0700

    Save off samples then print later.

--- src/main.cpp
+++ src/main.cpp
@@ -8,30 +8,49 @@
 #define COIL_B2 32 // Black wire in my setup  - D5 on my setup
 #define COIL_MASK (COIL_A | COIL_B | COIL_A2 | COIL_B2)
 
-byte old_pin_status = 0;
+#define SAMPLE_BUFFER_SIZE 1024
+
+byte sample_buffer[SAMPLE_BUFFER_SIZE] = {0};
+int next_sample = 0;
 
 void setup() {
   Serial.begin(115200);
-  for (int i = 2; i < 8; i++)
+
+  // This is very tied to my exact wiring.
+  for (int i = 2; i < 5; i++)
       pinMode(i, INPUT);
 }
 
 void loop() {
-  byte pin_status = PIND & COIL_MASK;
-  if (pin_status != old_pin_status) {
-    Serial.print("Port Status:");
-    Serial.println(pin_status, HEX);
-    if (pin_status & COIL_A)
-       Serial.println("A");
-    if (pin_status & COIL_B)
-       Serial.println("B");
-    if (pin_status & COIL_A2)
-       Serial.println("A2");
-    if (pin_status & COIL_B2)
-       Serial.println("B2");
+  byte pin_status, old_pin_status = 0;
 
-    old_pin_status = pin_status;
-  }
+  // First run the sampling loop
+  do {
+    pin_status = PIND & COIL_MASK;
+    if (pin_status != old_pin_status) {
+        sample_buffer[next_sample++] = pin_status;
+        old_pin_status = pin_status;
+    }
+  } 
+  while (next_sample < SAMPLE_BUFFER_SIZE - 1);
+
+  Serial.print("%i samples collected.\r\n");
+  Serial.println(SAMPLE_BUFFER_SIZE, HEX);
 
   delay(2);
+
+  for (int k = 0; k < SAMPLE_BUFFER_SIZE; k++) {
+    byte pin_status = sample_buffer[k];
+
+    Serial.print("Port:");
+    Serial.println(pin_status, HEX);
+    if (pin_status & COIL_A)
+    Serial.println("A");
+    if (pin_status & COIL_B)
+    Serial.println("B");
+    if (pin_status & COIL_A2)
+    Serial.println("A2");
+    if (pin_status & COIL_B2)
+    Serial.println("B2");
+  }
 }
